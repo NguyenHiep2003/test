92f8f7f860bb73bcee4080e9c4c4af124bc413a3
Nguyen Hiep <mccter2003@gmail.com>
2025-05-21 05:56:49 +0700

    KAN-1 New Commit 1

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,8 @@ import { AppModule } from './app.module';
 import { Logger, ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
-const a = 12345
+const a = 12345121
+const b = 6
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const port = process.env.PORT || 3000;
