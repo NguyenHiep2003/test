9b2f2ed202d2dcbfec21e7aea5e4b81f0e1f9c68
Nguyen Hiep <mccter2003@gmail.com>
2025-05-30 06:35:19 +0700

    SP-12 add feature payment

--- src/main.ts
+++ src/main.ts
@@ -8,24 +8,10 @@ const hehe = 1;
 const kkkk='kkk'
 const kkkk='kkk'
 
-const kkkk='kkk'
+const hello= 'hello'
+const bro= 'bro'
 
-const kkkk='kkk'
-const hehe = 1;
-const kkkk='kkk'
-const kkkk='kkk'
 
-const kkkk='kkk'
-
-const kkkk='kkk'
-const kkkk='kkk'
-const hehe = 1;
-const kkkk='kkk'
-const kkkk='kkk'
-
-const kkkk='kkk'
-
-const kkkk='kkk'
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const port = process.env.PORT || 3000;
